ca2e39ee748e4ea73e162eefe4a357dc9784aee7
tessabass01 <tessabri.bass@gmail.com>
2024-03-28 09:00:23 -0600

    changes I've made to the dropdown (not done yet)

--- components/dropdown.js
+++ components/dropdown.js
@@ -1,8 +1,14 @@
 import { useState } from "react";
+import catData from "../data/cleanedTraitData.json";
 
 const Dropdown = () => {
+
+
+
   const [country, setCountry] = useState("");
   const [state, setState] = useState("");
+  const [categories, setCat] = useState(Object.keys(catData[0]));
+  const [categories2, setCat2] = useState("");
 
   const handleFilterChange = (e) => {
     setCountry(e.target.value);
@@ -13,24 +19,19 @@ const Dropdown = () => {
     setState(e.target.value);
   };
 
-  if (country === "Habitat") {
+  if (country === "habitat") {
   return (
     <div>
       <select className="px-4 py-3 text-lg font-medium text-center text-gray-500 rounded-md" onChange={handleFilterChange} value={country}>
         <option disabled selected value="">-- Select Filter --</option>
-        <option value="Body Mass">Body Mass</option>
-        <option value="Habitat">Habitat</option>
+        {categories.map(cat => (
+                  <option value={cat}>{cat}</option>
+                ))}
       </select>
       <select className="px-4 py-3 text-lg font-medium text-center text-gray-500 rounded-md" onChange={handleSecondFilterChange} value={state} disabled={country === ""}>
                 <option value="">-- Select Habitat --</option>
-                {country === "Habitat" && (
-                <><option key="Forest">Forest</option><option key="Ocean">Ocean</option><option key="Ocean">Savannah</option></>
-                )}
-                {/* {country === "USA" && (
-                <><option key="California">California</option><option key="Texas">Texas</option><option key="New York">New York</option></>
-                )}
-                {country === "UK" && (
-                <><option key="London">London</option><option key="Manchester">Manchester</option><option key="Birmingham">Birmingham</option></>
+                {/* {country === "habitat" && (
+                <><option key="Forest">Forest</option><option key="Ocean">Ocean</option><option key="Savannah">Savannah</option></>
                 )} */}
             </select>
     </div>
@@ -40,8 +41,11 @@ const Dropdown = () => {
             <div>
               <select className="px-4 py-3 text-lg font-medium text-center text-gray-500 rounded-md" onChange={handleFilterChange} value={country}>
                 <option disabled selected value="">-- Select Filter --</option>
-                <option value="Body Mass">Body Mass</option>
-                <option value="Habitat">Habitat</option>
+                {/* <option value="Body Mass">Body Mass</option>
+                <option value="Habitat">Habitat</option> */}
+                {categories.map(cat => (
+                  <option value={cat}>{cat}</option>
+                ))}
               </select>
               </div>
     )};

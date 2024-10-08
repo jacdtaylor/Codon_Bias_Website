6b48eb64f4001f233682c4a94661a2f382219738
jac <jacdtaylor@gmail.com>
2024-10-08 13:07:46 -0600

    First working compare Ortho

--- pages/compareOrtho.js
+++ pages/compareOrtho.js
@@ -2,109 +2,149 @@ import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 import Navbar from "../components/navbar";
 import Head from "next/head";
-import { ClusterCodonData } from "../components/cluster.js";
 import { saveAs } from 'file-saver';
-import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
-import allSpeciesData from "../data/speciesList.json"
+import allSpeciesData from "../data/speciesList.json";
+import Order from "../data/codonOrder.json"
+import { drawChart } from '../components/orthoHeatMap';
 
 const compareOrtho = () => {
 
     const svgRef = useRef();
-    const [species, setSpecies] = useState("")
-    const [selectedGenes, setSelectedGenes] = useState([])
-    const [proportionData, setProportionData] = useState([])
-    const [allSpecies, setAllSpecies] = useState([])
-    const [currentSpeciesData, setCurrentSpeciesData] = useState({})
+    const [codonOrder,setCodonOrder] = useState([])
+    const [species, setSpecies] = useState("");
+    const [allSpecies, setAllSpecies] = useState([]);
+    const [currentSpeciesData, setCurrentSpeciesData] = useState({});
+    const [currentGenes, setCurrentGenes] = useState([]);
+    const [selectedGenes, setSelectedGenes] = useState([]);
+    const [newId, setNewId] = useState("");
 
-    useEffect(()=>{
+    useEffect(() => {
         setAllSpecies(allSpeciesData);
+        setCodonOrder(Order);
     }, []);
 
-
-    
-
     const downloadGraph = () => {
-        // Select the SVG element
         const svgElement = svgRef.current;
-    
-        // Get the SVG XML string
         const svgXML = new XMLSerializer().serializeToString(svgElement);
-    
-        // Create an image element
         const img = new Image();
-    
-        // Set the image source to the SVG XML
         img.src = 'data:image/svg+xml;base64,' + btoa(svgXML);
-    
-        // When the image loads
+
         img.onload = () => {
-            // Create a canvas element
             const canvas = document.createElement('canvas');
             const context = canvas.getContext('2d');
-    
-            // Set canvas size to match SVG size
             canvas.width = svgElement.clientWidth;
             canvas.height = svgElement.clientHeight;
-    
-            // Draw the image onto the canvas
             context.drawImage(img, 0, 0);
-    
-            // Convert canvas to blob
             canvas.toBlob(blob => {
-                // Save blob as file using FileSaver.js
                 saveAs(blob, 'graph.png');
             });
         };
     };
 
-
-
     const handleSpeciesChange = (e) => {
-        const selected = e.target.species;
+        const selected = e.target.value;
         setSpecies(selected);
-        console.log()
         handleDataChange(selected);
+    };
+
+    const handleInputChange = (e) => {
+        setNewId(e.target.value);
+    };
+
+    const handleGeneChange = (e) => {
+        const selected = e;
+        const proportionData = currentSpeciesData[selected][1];
+        const AddedData = Order.reduce((acc, key, index) => {
+            acc[key] = proportionData[index];
+            return acc;
+          }, {});
+
+        AddedData["Species"] = species;
+        AddedData["Gene"] = selected;
+        // const AddedData = {"Species":species,"Gene":selected, "Data":currentSpeciesData[selected]}
+        setSelectedGenes([AddedData, ...selectedGenes]);
     }
 
 
     const handleDataChange = (item) => {
-        console.log(`speciesIndividualJSONS/${item}JSON.json`)
-        fetch(`speciesIndividualJSONS/${item}JSON.json`).then(response => {
-            if (!response.ok) {throw new Error("Species Error");}
-            return response.json();
+        fetch(`speciesIndividualJSONS/${item}JSON.json`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch species data");
+                }
+                return response.json();
+            })
+            .then(data => {
+                setCurrentSpeciesData(data);
+                setCurrentGenes(Object.keys(data))
+            })
+            .catch(error => {
+                console.error("Error fetching species data:", error);
+            });
+    };
 
-        })
-        .then(data=>{
-            setCurrentSpeciesData(data);
-            console.log(data)
-        })
+    const HandleGraph = () => {
+        drawChart(selectedGenes, svgRef)
     }
-
-
-    
 
     return (
         <>
         <link rel="stylesheet" href="filter.css"></link>
-            <Head>
-            </Head>
-            <Navbar />  
-            <select
+        <Head></Head>
+        <Navbar />
+        <select
             className="px-4 py-3 text-lg font-medium text-center text-gray-500 rounded-md"
             onChange={handleSpeciesChange}
             value={species}
-          >
+        >
             <option disabled value="">
               -- Select Species --
             </option>
-            {allSpecies
-              .map((cat) => (
+            {allSpecies.map((cat) => (
                 <option key={cat} value={cat}>
                   {cat}
                 </option>
-              ))}
-          </select>
+            ))}
+        </select>
+        {/* <select
+            className="px-4 py-3 text-lg font-medium text-center text-gray-500 rounded-md"
+            onChange={handleGeneChange}
+            // value={Gene}
+        >
+            <option disabled value="">
+              -- Select Gene --
+            </option>
+            {currentGenes.map((cat) => (
+                <option key={cat} value={cat}>
+                  {cat}
+                </option>
+            ))}
+        </select> */}
+
+        <div className="input-container">
+            <input type="text" value={newId} onChange={handleInputChange} placeholder="Enter ID" />
+        </div>
+        <ul className="GeneNamesUl">
+        {currentGenes
+            .filter(id => id.toLowerCase().startsWith(newId.toLowerCase()))
+            .slice(0, 30)
+            .map((id, index) => (
+                <li className="GeneNamesLi" key={index} onClick={() => handleGeneChange(id)}>
+                    {id}
+                </li>
+            ))}
+    </ul>
+        <button onClick={HandleGraph}>Plot</button>
+        <container className="G_container">
+            <container className="Graph">
+            <svg ref={svgRef}></svg>
+            </container>
+            </container>
+                <div id="info-box" ></div>
+
+        <button onClick={downloadGraph}>Download Graph</button>
         </>
     );
-}
+};
+
 export default compareOrtho;

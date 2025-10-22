e0cb69a1da6824da5b2fc529911659d27fb8a9c5
mattcloward <mattcloward@gmail.com>
2025-10-22 15:39:16 -0600

    Updated index.js description

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
 import Head from "next/head";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
-import SectionTitle from "../components/sectionTitle";
-
-import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
-import Benefits from "../components/benefits";
-import Footer from "../components/footer";
-import Testimonials from "../components/testimonials";
-import Cta from "../components/cta";
-import Faq from "../components/faq";
-import PopupWidget from "../components/popupWidget";
 
 const Home = () => {
   return (
@@ -19,7 +9,7 @@ const Home = () => {
         <title>CUBhub</title>
         <meta
           name="description"
-          content="This is our website now!"
+          content="Codon Usage Bias Hub: for all your CUB analyses!"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
